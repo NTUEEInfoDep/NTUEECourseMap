894e099592d483338afeadc8dfaf4939a454ccb3
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-05-10 10:40:02 +0800

    upgrade dependencies to support Node v16.15.1, fix bugs for all import scss error

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import useBlogData from "../static_queries/useBlogData"
-import blogListStyles from "../styles/components/bloglist.module.scss"
+import * as blogListStyles from "../styles/components/bloglist.module.scss"
 import Img from "gatsby-image"
 
 export default function BlogList() {
