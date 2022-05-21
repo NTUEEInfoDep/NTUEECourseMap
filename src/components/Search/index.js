0bba72c0f962d0223bee345b44f89ad65d2cc9de
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-05-21 13:39:28 +0800

    add Search.js for search drawer and finish SortTool component

---
+++ src/components/Search/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react"
+
+// mui
+import Button from "@mui/material/Button"
+import SwipeableDrawer from "@mui/material/SwipeableDrawer"
+import SearchIcon from "@mui/icons-material/Search"
+import { styled } from "@mui/material/styles"
+
+// components
+import SortTool from "./SortTool"
+
+const sortList = [
+  {
+    value: "date",
+    label: "時間",
+  },
+  {
+    value: "score",
+    label: "評分",
+  },
+  {
+    value: "semester",
+    label: "學期",
+  },
+  {
+    value: "like",
+    label: "推文數",
+  },
+]
+
+// Customize the open button for search drawer
+const SearchButton = styled(Button)({
+  position: "absolute",
+  left: "0",
+  top: "20%",
+  width: "50px",
+  height: "80px",
+  background: "#9CD0FF",
+  borderRadius: "0% 10% 10% 0%",
+})
+
+export default function Search() {
+  const [openSearch, setOpenSearch] = useState(false)
+  const [sortValue, setSortValue] = useState("date")
+  const [sortOrder, setSortOrder] = useState("ascending")
+
+  const handleOpenSearch = () => {
+    setOpenSearch(true)
+  }
+  const handleCloseSearch = () => {
+    setOpenSearch(false)
+  }
+
+  return (
+    <React.Fragment key="search">
+      <SearchButton onClick={handleOpenSearch}>
+        <SearchIcon sx={{ fontSize: "30px", color: "#ffffff" }} />
+      </SearchButton>
+      <SwipeableDrawer
+        open={openSearch}
+        onClose={handleCloseSearch}
+        onOpen={handleOpenSearch}
+      >
+        <SortTool
+          sortList={sortList}
+          sortValue={sortValue}
+          setSortValue={setSortValue}
+          sortOrder={sortOrder}
+          setSortOrder={setSortOrder}
+        />
+      </SwipeableDrawer>
+    </React.Fragment>
+  )
+}
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+})
+
+export default theme
